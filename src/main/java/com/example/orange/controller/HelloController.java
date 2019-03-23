fb8265c2ebbb4705e3caf257c14ebd4364a78f19
ymf <468490076@qq.com>
2019-03-24 00:19:47 +0800

    提交

---
+++ src/main/java/com/example/orange/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.orange.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(String str){
+        return "hello world";
+    }
+}
